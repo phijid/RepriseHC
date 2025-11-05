c67077909316c48aa31878e7f5d787272a8b37c4
Phijid <mannysran@hotmail.com>
2025-11-04 20:54:13 -0500

    A Humble Task

--- Achievements.lua
+++ Achievements.lua
@@ -386,6 +386,7 @@ end
 local QUEST_MILESTONES = {
   { questId = 363, levelCap = 10,  title = "Rude Awakening", faction = "Horde" },
   { questId = 4641, levelCap = 10,  title = "Your Place In The World", faction = "Horde" },
+  { questId = 753, levelCap = 10,  title = "A Humble Task", faction = "Horde" },
   { questId = 2561, levelCap = 9,  title = "Druid of the Claw", faction = "Alliance" },  
   { questId = 314,  levelCap = 10, title = "Protecting the Herd", faction = "Alliance" },
   { questId = 176,  levelCap = 11, title = "Wanted: Hogger", faction = "Alliance" },
